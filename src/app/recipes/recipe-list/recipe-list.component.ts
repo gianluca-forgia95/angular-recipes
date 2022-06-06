8c98b93cfd8d54cad95dc929e931445554afdda0
enet-gforgia <87761260+enet-gforgia@users.noreply.github.com>
2022-06-06 16:44:47 +0200

    header + nest dei componenti

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('Test Recipy', 'example of description of a recipy', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg' ),
+    new Recipe('Test Recipy', 'example of description of a recipy', 'https://img.delicious.com.au/fVd1u6k7/w1200/del/2022/02/chicken-chickpea-curry-163942-1.jpg' )
+  ]
 
   constructor() { }
 
