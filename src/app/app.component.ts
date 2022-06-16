932ed0d106d22c7be3b4257deaa55777b9ad47f2
enet-gforgia <87761260+enet-gforgia@users.noreply.github.com>
2022-06-16 10:48:41 +0200

    navigation con ngIf, input output events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_recipes';
+
+  loadedFeature = "recipes"
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature
+  }
 }
